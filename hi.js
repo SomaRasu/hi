37db0f825d88a7de20272956aae8078cdb65b1c6
SomaRasu <mhaynes1337@gmail.com>
2015-03-27 01:58:43 -0700

    Create hi.js

---
+++ hi.js
@@ -0,0 +1,9 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/my-app/sw.js', {
+    scope: '/my-app/'
+  }).then(function(reg) {
+    console.log('Yey!', reg);
+  }).catch(function(err) {
+    console.log('Boo!', err);
+  });
+}
